ac41609ad5406524bcbe2d39817f1e0450e7c307
nmorse <morse.nate@gmail.com>
2014-06-17 22:59:37 -0400

    acheved an embedded recursive dynamic template, go-meta

--- js2/app.js
+++ js2/app.js
@@ -3,6 +3,7 @@ var app = angular.module('myApp', []);
 app.directive('viewItem', function ($compile) {
     var radioSetTemplate = '<div class="entry-photo"><span>{{content.title}}"</span><div ng-repeat="item in content.data" content="item">{{item.name}}</div></div>';
     var textareaTemplate = '<div class="entry-video">{{content.data}}</div>';
+    var metaTemplate = '<div class="entry-video">{{content.title}}<view-item ng-repeat="item in content.data" content="item"></view-item></div>';
     var textTemplate = '<div class="entry-note"><h2>{{content.title}}</h2>{{content.data}}</div>';
 
     var getTemplate = function(viewType) {
@@ -14,6 +15,9 @@ app.directive('viewItem', function ($compile) {
             case 'textarea':
                 template = textareaTemplate;
                 break;
+            case 'meta':
+                template = metaTemplate;
+                break;
             case 'text':
                 template = textTemplate;
                 break;
@@ -22,7 +26,6 @@ app.directive('viewItem', function ($compile) {
     }
 
     var linker = function(scope, element, attrs) {
-        scope.rootDirectory = 'images/';
         element.html(getTemplate(scope.content.view_type));
         $compile(element.contents())(scope);
     }
@@ -42,6 +45,7 @@ function ViewCtrl($scope, $http) {
     $scope.content = [
         {"view_type": "radio set", "title": "Radio ABC"   , "data" : [{"name":"A"}, {"name":"B", "selected":true}, {"name":"C"}]},
         {"view_type": "textarea" , "title": "Big Text 000", "data" : "http://player.vimeo.com/video/37176398"},
+        {"view_type": "meta" , "title": "Meta", "data" : [{"view_type": "radio set", "title": "Radio CDF"   , "data" : [{"name":"C"}, {"name":"D", "selected":true}, {"name":"F"}]}, {"view_type": "text"     , "title": "Inner Notes 000"   , "data" : "Lorem ipsum"}]},
         {"view_type": "text"     , "title": "Notes 000"   , "data" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh felis, condimentum sit amet laoreet luctus, posuere auctor lorem. Nullam malesuada."},
     ];
 }
