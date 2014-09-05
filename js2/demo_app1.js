d107f18c3fc2324e0de146e73f40d5dc07b8013f
Nate Morse <morse.nate@gmail.com>
2014-09-05 00:00:31 -0400

    simple enough

--- js2/demo_app1.js
+++ js2/demo_app1.js
@@ -35,7 +35,7 @@ app.directive('drivenTemplate', function ($compile) {
     };
 
     var linker = function(scope, element, attrs) {
-        element.html(getTemplate(scope.content.view_type));
+        element.html(getTemplate(scope.content.view_template));
         $compile(element.contents())(scope);
     };
 
@@ -53,18 +53,18 @@ app.directive('drivenTemplate', function ($compile) {
 function ViewCtrl($scope, $http) {
     "use strict";
     $scope.content = {"data":[
-        {"view_type": "section", "title": "My First Section", "narrative": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh felis, condimentum sit amet laoreet luctus, posuere auctor lorem. Nullam malesuada."},
-        {"view_type": "table" , "title": "A First Table", "data" : [["COS 171", "Makeit or Breakit Data Structures", 3], ["COS 222", "Really Boring Data Structures", 3], ["COS 234", "Sequence Recognition in Data", 5]
+        {"view_template": "section", "title": "My First Section", "narrative": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh felis, condimentum sit amet laoreet luctus, posuere auctor lorem. Nullam malesuada."},
+        {"view_template": "table" , "title": "A First Table", "data" : [["COS 171", "Makeit or Breakit Data Structures", 3], ["COS 222", "Really Boring Data Structures", 3], ["COS 234", "Sequence Recognition in Data", 5]
 			]},
-        {"view_type": "section", "title": "Second Section", "narrative": "Lorem ipsum blah blah blah dolor sit amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh felis, condimentum sit amet laoreet luctus, posuere auctor lorem. Nullam malesuada."},
-        {"view_type": "table with narrative" , "title": "A First Table", "data" : [["MAT 123", "Four Topics in Math", 3], ["MAT 456", "More Topics in Math", 6], ["MAT 789", "Studies in Sequences", 9]
+        {"view_template": "section", "title": "Second Section", "narrative": "Lorem ipsum blah blah blah dolor sit amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh felis, condimentum sit amet laoreet luctus, posuere auctor lorem. Nullam malesuada."},
+        {"view_template": "table with narrative" , "title": "A First Table", "data" : [["MAT 123", "Four Topics in Math", 3], ["MAT 456", "More Topics in Math", 6], ["MAT 789", "Studies in Sequences", 9]
 			], "narrative": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh"},
-        {"view_type": "footer", "narrative": "Lorem ipsum footer ipsum dolor sit footer amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh"}
+        {"view_template": "footer", "narrative": "Lorem ipsum footer ipsum dolor sit footer amet, consectetur adipiscing elit. Nunc pulvinar pretium felis. Vivamus nibh"}
     ]};
     $scope.stringify_content = function () {
         return JSON.stringify($scope.content, null, ' ');
     };
     $scope.add_content = function () {
-        $scope.content.data.unshift( {"view_type": "section", "title": "A New First Section", "narrative": "Lorem ipsum nan nan-nan nan-nan nahh dolor sit amet, consectetur adipiscing elit."} );
+        $scope.content.data.unshift( {"view_template": "section", "title": "A New First Section", "narrative": "Lorem ipsum nan nan-nan nan-nan nahh dolor sit amet, consectetur adipiscing elit."} );
     };
 }
